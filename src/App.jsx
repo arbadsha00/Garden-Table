42b2238a12ab60a6a343d9fd4ff052efe072e66f
arbadsha00 <arbadsha00@gmail.com>
2024-10-22 20:58:18 +0600

    our recepies text done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./Components/Header";
+import OurRecipes from "./Components/OurRecipes";
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <OurRecipes></OurRecipes>
     </div>
   );
 };
---
+++ src/Components/OurRecipes.jsx
@@ -0,0 +1,12 @@
+
+
+const OurRecipes = () => {
+    return (
+        <div className="container mx-auto text-center space-y-3 my-10 px-5">
+            <h1 className="font-semibold text-2xl md:text-4xl">Our Recipes</h1>
+            <p className="text-gray-500 md:w-4/5 mx-auto text-xs md:text-base">A Collection of Timeless Culinary Creations to Delight Your Taste Buds and Bring Joy to Every Meal. These Recipes Will Elevate Your Everyday Cooking Experience.</p>
+        </div>
+    );
+};
+
+export default OurRecipes;

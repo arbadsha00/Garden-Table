c5026c18446c5e36d423679ff52cc4e73df87a92
arbadsha00 <arbadsha00@gmail.com>
2024-10-23 12:53:08 +0600

    currently cooking section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,46 @@ import OurRecipes from "./Components/OurRecipes";
 import Recipes from "./Components/Recipes";
 import Cooking from "./Components/Cooking";
 import { useState } from "react";
+
 const App = () => {
   const [wantToCooks, setWantToCooks] = useState([]);
+  const [totalTime, setTotalTime] = useState(0);
+  const [totalCalories, setTotalCalories] = useState(0);
 
   function handleWantToCook(recipe) {
     if (wantToCooks.includes(recipe) == 0) {
       setWantToCooks([...wantToCooks, recipe]);
+    } else {
+      alert("one recipe at a time");
     }
   }
-  console.log(wantToCooks);
+  const [cooking, setCooking] = useState([]);
+
+  function updateTimeCalories(time, calories) {
+    setTotalTime(totalTime + time);
+    setTotalCalories(totalCalories + calories);
+  }
+  function handleCooking(recipe) {
+    setCooking([...cooking, recipe]);
+
+    setWantToCooks(wantToCooks.filter((item) => item !== recipe));
+
+    updateTimeCalories(recipe.cooking_time_minutes, recipe.calories);
+  }
+
   return (
     <div>
       <Header></Header>
-      <OurRecipes ></OurRecipes>
+      <OurRecipes></OurRecipes>
       <section className="grid grid-cols-1  lg:grid-cols-3 container mx-auto px-4 gap-5">
         <Recipes handleWantToCook={handleWantToCook}></Recipes>
-        <Cooking wantToCooks={wantToCooks}></Cooking>
+        <Cooking
+          wantToCooks={wantToCooks}
+          handleCooking={handleCooking}
+          cooking={cooking}
+          totalTime={totalTime}
+          totalCalories={totalCalories}
+        ></Cooking>
       </section>
     </div>
   );
--- src/Components/Cooking.jsx
+++ src/Components/Cooking.jsx
@@ -1,7 +1,11 @@
-const Cooking = ({ wantToCooks }) => {
+import { useState } from "react";
+
+const Cooking = ({ wantToCooks, cooking, handleCooking,totalTime,totalCalories }) => {
+    
+
   return (
-    <div>
-      <section className="mx-auto rounded-xl border-2">
+    <div className="className=mx-auto rounded-xl border-2">
+      <section>
         <h1 className="font-semibold text-2xl text-center mt-5">
           Want to Cook : {wantToCooks.length}
         </h1>
@@ -19,20 +23,62 @@ const Cooking = ({ wantToCooks }) => {
               </tr>
             </thead>
             <tbody>
-             {
-                wantToCooks.map((recipe,idx)=><tr key={idx}  className="hover text-gray-500 text-xs">
-                    <th>{ idx+1}</th>
-                <td>{recipe.recipe_name}</td>
-                <td>{recipe.cooking_time_minutes} minutes</td>
-                <td>{recipe.calories} calories</td>
-                <td><button className=" p-2 bg-primary text-black border-none rounded-full hover:bg-black hover:text-primary text-xs">
-            Preparing
-          </button></td>
-              </tr>)
-             }
-              
-              
+              {wantToCooks.map((recipe, idx) => (
+                <tr key={idx} className="hover text-gray-500 text-xs">
+                  <th>{idx + 1}</th>
+                  <td>{recipe.recipe_name}</td>
+                  <td>{recipe.cooking_time_minutes} minutes</td>
+                  <td>{recipe.calories} calories</td>
+                  <td>
+                    <button
+                      onClick={() => handleCooking(recipe)}
+                      className=" p-2 bg-primary text-black border-none rounded-full hover:bg-black hover:text-primary text-xs"
+                    >
+                      Preparing
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
+          </table>
+        </div>
+      </section>
+
+      <section>
+        <h1 className="font-semibold text-2xl text-center mt-5">
+          Currently Cooking : {cooking.length}
+        </h1>
+        <div className="divider"></div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Time</th>
+                <th>Calories</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cooking.map((recipe, idx) => (
+                <tr key={idx} className="hover text-gray-500 text-xs">
+                  <th>{idx + 1}</th>
+                  <td>{recipe.recipe_name}</td>
+                  <td>{recipe.cooking_time_minutes} minutes</td>
+                  <td>{recipe.calories} calories</td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <th></th>
+                <th></th>
+                              <th>Total Time : {totalTime }</th>
+                              <th>Total Calories : {totalCalories }</th>
+                <th></th>
+              </tr>
+            </tfoot>
           </table>
         </div>
       </section>
